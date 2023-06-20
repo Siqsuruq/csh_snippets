5f656d4787ed87fea77c3266e70da69b775f6942
Siqsuruq <siqsuruq@gmail.com>
2023-06-20 16:46:26 -0100

    WIP

--- REST/Program.cs
+++ REST/Program.cs
@@ -1,52 +1,53 @@
-using Microsoft.Extensions.Hosting;
-using CloudNative.CloudEvents;
-using CloudNative.CloudEvents.Http;
-using CloudNative.CloudEvents.NewtonsoftJson;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
 
-namespace REST
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureKestrel(serverOptions =>
 {
-    public class Program
+    // Certificate from File
+    GlobalVariables.CertificateInfo = "File";
+    //serverOptions.ConfigureHttpsDefaults(listenOptions =>
+    //{
+    //    listenOptions.ServerCertificate = new X509Certificate2("C:\\Users\\Max\\Documents\\Certificate\\certificate.pfx", "123");
+    //});
+    serverOptions.Listen(IPAddress.Any, 5001, listenOptions =>
     {
-        public static void Main(string[] args)
-        {
-            var host = CreateHostBuilder(args).Build();
-            host.Run();
-        }
+        listenOptions.UseHttps();
+    });
+
+    // Certificate from Personal Store
+    //GlobalVariables.CertificateInfo = "Personal Store";
+    //serverOptions.Listen(IPAddress.Any, 5001, listenOptions =>
+    //{
+    //    using (var store = new X509Store(StoreName.My, StoreLocation.LocalMachine))
+    //    {
+    //        store.Open(OpenFlags.ReadOnly);
+
+    //        var certs = store.Certificates.Find(X509FindType.FindByIssuerName, "MaComutor", validOnly: false);
+
+    //        if (certs.Count > 0)
+    //        {
+    //            var cert = certs[0];
+    //            listenOptions.UseHttps(cert);
+    //        }
+    //        else
+    //        {
+    //            throw new Exception("No certificate found with thumbprint: <thumbprint>");
+    //        }
+    //    }
+    //});
+});
 
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .UseWindowsService()
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.Configure(app =>
-                    {
-                        var env = app.ApplicationServices.GetRequiredService<IWebHostEnvironment>();
+var app = builder.Build();
+var a = GlobalVariables.CertificateInfo;
+string WherIsCert = $"Hello World! Certificate is from the {a}";
+app.MapGet("/", () => WherIsCert);
+app.Run();
 
-                        // Configure the HTTP request pipeline.
-                        if (env.IsDevelopment())
-                        {
-                            app.UseDeveloperExceptionPage();
-                        }
 
-                        app.UseHttpsRedirection();
-                        app.UseAuthorization();
-                        app.UseRouting();
-                        app.UseEndpoints(endpoints =>
-                        {
-                            endpoints.MapControllers();
-                        });
-                    });
-                })
-                .ConfigureServices((hostContext, services) =>
-                {
-                    // Add controllers
-                    services.AddControllers();
-
-                    // Configure CloudEvent serialization and deserialization
-                    services.AddSingleton<CloudEventFormatter, JsonEventFormatter>();
-                    services.AddHttpContextAccessor();
-                });
-
-    }
+public static class GlobalVariables
+{
+    // Create a static field
+    public static string? CertificateInfo;
 }
---
+++ StorageEmulator/StorageEmulator/StorageEmulator/StorageEmulator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace StorageEmulator
+{
+    internal class StorageEmulator
+    {
+        private string filePath = "EmulatorData.json";
+        private Dictionary<string, object> jsonData;
+
+        public void JsonReadWrite(string filePath)
+        {
+            this.filePath = filePath;
+            jsonData = ReadJsonData();
+        }
+
+        public void WriteToJson(string key, object value)
+        {
+            // Update or add the specified key-value pair
+            jsonData[key] = value;
+
+            // Serialize the updated JSON data
+            string jsonString = JsonSerializer.Serialize(jsonData);
+
+            // Write the JSON string back to the file
+            File.WriteAllText(filePath, jsonString);
+        }
+
+        public object ReadFromJson(string key)
+        {
+            // Retrieve the value for the specified key
+            if (jsonData.ContainsKey(key))
+            {
+                return jsonData[key];
+            }
+
+            // Key not found
+            return null;
+        }
+
+        private Dictionary<string, object> ReadJsonData()
+        {
+            try
+            {
+                // Read the entire JSON file as a string
+                string jsonString = File.ReadAllText(filePath);
+
+                // Deserialize the JSON string into a dictionary
+                return JsonSerializer.Deserialize<Dictionary<string, object>>(jsonString);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("The file could not be found.");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred while reading the file: " + e.Message);
+            }
+            catch (JsonException e)
+            {
+                Console.WriteLine("An error occurred while parsing the JSON: " + e.Message);
+            }
+
+            return new Dictionary<string, object>();
+        }
+    }
+}
